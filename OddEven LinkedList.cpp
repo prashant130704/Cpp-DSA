51d78759cef8e377c1d014ad8e687cf556843496
prashant130704 <153508066+prashant130704@users.noreply.github.com>
2026-01-29 01:19:07 +0530

    oddEvenList function for linked list
    Implement odd-even linked list reordering.

---
+++ OddEven LinkedList.cpp
@@ -0,0 +1,53 @@
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode *Dummy=new ListNode();
+        Dummy->next=head;
+        ListNode *odd=nullptr;
+        ListNode *even=nullptr;
+        ListNode *Connection=nullptr;
+        ListNode *temp=head;
+        int cnt=1;
+        while(temp!=nullptr){
+            //ListNode *nextnode=temp->next;
+            if(cnt%2!=0){
+                if(odd==nullptr){
+                    odd=temp;
+                }
+                else{
+                    odd->next=temp;
+                    odd=odd->next;
+                }
+            }
+            else{
+                if(even==nullptr){
+                    even=temp;
+                    Connection=temp;
+                }
+                else{
+                    even->next=temp;
+                    even=even->next;
+                }
+            }
+            cnt++;
+            temp=temp->next;
+        }
+        odd->next=Connection;
+        even->next=nullptr;
+        return Dummy->next;
+
+    }
+};
